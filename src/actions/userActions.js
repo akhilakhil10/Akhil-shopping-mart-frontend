e76b29341244b7979c2da5c3ce79b6d2d340c73e
SriAkhilReddy <medapatisriakhilreddy@gmail.com>
2023-05-02 17:29:12 +0530

    commit

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -107,8 +107,7 @@ export const loadUser = () => async (dispatch) => {
     try {
 
         console.log('user request')
-        dispatch({ type: LOAD_USER_REQUEST })
-
+        // dispatch({ type: LOAD_USER_REQUEST })
         const { data } = await axios.get(`https://akhil-shopping-mart-a.onrender.com/api/v1/me`)
         console.log(data,'load')
         console.log(data);
