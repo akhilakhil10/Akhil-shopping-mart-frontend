c92da7fe4ed64b55d2f1963e2b169c92bbd9d18f
Sri Akhil Reddy <109200566+akhilakhil10@users.noreply.github.com>
2023-05-03 23:18:20 +0530

    Update userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -139,7 +139,7 @@ export const loadUser = () => async (dispatch) => {
         };
 
         console.log('config',config)
-        const { data } = await axios.get('/api/v1/me');
+        const { data } = await axios.get('/api/v1/me',config);
 
         dispatch({
             type: LOAD_USER_SUCCESS,
