0f3b5b47d162a92728beec3388edc7300ca38d00
Sri Akhil Reddy <109200566+akhilakhil10@users.noreply.github.com>
2023-05-09 10:08:20 +0530

    Update userReducers.js

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -55,7 +55,7 @@ export const authReducer = (state = { user: {} }, action) => {
         case LOAD_USER_REQUEST:
             console.log("user request");
             return {
-                loading:true,
+                loading:false,
                 isAuthenticated: false,
             }
 
